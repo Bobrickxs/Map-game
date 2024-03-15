08e25c9d4a86f8a5e83917588d1c96982b2c2da5
Bobrickxs <azachemetovsyo@gmail.com>
2024-03-15 18:41:20 +0200

    New game with pirat map

---
+++ scripts1.js
@@ -0,0 +1,53 @@
+
+  function getRandomNumber(size) {
+
+    let num = Math.floor(Math.random() * size);
+    return num;
+}
+// Розраховує відстань від кліку (event) до скарбу (target)
+function getDistance() {
+    const diffX = event.offsetX - target.x;
+    const diffY = event.offsetY - target.y;
+    const dist = Math.sqrt((diffX * diffX) + (diffY * diffY))
+    return dist;
+}
+
+function getDistanceHind(distance) {
+    if (distance < 10) {
+        return "Пече!";
+    } else if (distance < 20) {
+      return "Дуже гаряче";
+    } else if (distance < 40) {
+        return "Гаряче";
+    } else if (distance < 80) {
+        return "Тепло";
+    } else if (distance < 160) {
+        return "Холодно";
+    } else if (distance < 320) {
+        return "Дуже холодно";
+    } else {
+        return "Можно замерзнути!";
+    }
+}
+
+const width = 400;
+const height = 400;
+let click = 0;
+
+const target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+}
+
+const mapElement = document.getElementById("map");
+mapElement.addEventListener("click", (event) => {
+    click++;
+    ///console.log(click)
+    const distance = getDistance(event, target);
+    const distanceHint = getDistanceHind(distance);
+    const distanceHintElement = document.getElementById("distance-hint");
+    distanceHintElement.textContent = distanceHint;
+    if (distance < 8)  {
+        alert("В точку!")
+    }
+  });
